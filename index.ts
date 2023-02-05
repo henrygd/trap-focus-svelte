5b436a0afc3e942ad97d7d85ef43a3a35281fb06
hank <hank@henrygd.me>
2023-02-05 14:14:13 -0600

    rewrite to add / remove from stack on update & account for dynamic content changes

--- index.ts
+++ index.ts
@@ -1,76 +1,80 @@
 import { listen } from 'svelte/internal'
 
-interface TrapOptions {
-	/** enables or disables wrap */
-	active?: boolean
-	/** wrapper element */
-	wrap?: HTMLElement
-}
-
 let stack: HTMLElement[] = []
 
 // true if tabbing backwards with shift + tab
 let shiftTab = false
 
-listen(document, 'keydown', (e: KeyboardEvent) => {
-	shiftTab = e.shiftKey && e.key === 'Tab'
-})
+listen(document, 'keydown', (e: KeyboardEvent) => (shiftTab = e.shiftKey && e.key == 'Tab'))
 
 /** Traps focus within a wrapper element */
-function trapFocus(node: HTMLElement, options: TrapOptions = { active: true }) {
-	let { wrap = node, active } = options
+function trapFocus(wrap: HTMLElement, active = true) {
+	// return  the first and last focusable children
+	function getFirstAndLastFocusable() {
+		const els = [...wrap.querySelectorAll('*')].filter(
+			(element: HTMLElement) => element.tabIndex >= 0
+		)
+		return [els.at(0) ?? wrap, els.at(-1) ?? wrap] as HTMLElement[]
+	}
 
-	const focusableEls = [...wrap.querySelectorAll('*')].filter(
-		(element: HTMLElement) => element.tabIndex >= 0
-	) as HTMLElement[]
+	// store document.activeElement to restore focus when untrapped
+	let lastActiveElement: HTMLElement
 
-	const firstFocusableEl = focusableEls.at(0) ?? wrap
-	const lastFocusableEl = focusableEls.at(-1) ?? wrap
-	const lastActiveElement = document.activeElement as HTMLElement
+	/** activates trap (adds to stack) and focuses inside */
+	function addToStack() {
+		stack.push(wrap)
+		lastActiveElement = document.activeElement as HTMLElement
+		getFirstAndLastFocusable().at(0).focus()
+	}
+	/** deactivates trap (removes from stack) and restores focus to lastActiveElement */
+	function removeFromStack() {
+		stack = stack.filter((el) => el != wrap)
+		lastActiveElement.focus()
+	}
 
-	// add to stack
-	stack.push(wrap)
+	// add to stack if active
+	if (active) {
+		addToStack()
+	}
 
-	// set initial focus on first focusable el
-	firstFocusableEl.focus()
+	/** true if element is in the trap most recently added to stack */
+	const inCurrentTrap = (el: HTMLElement) => stack.at(-1)?.contains(el)
 
-	/** true if element is in the last trap added to stack */
-	const inCurrentTrap = (el: HTMLElement = wrap) => stack.at(-1).contains(el)
-
-	// use blur listeners to redirect focus before it leaves container\
-	/** focus last element if focus leaves first element with shift */
-	const firstElBlurListener = listen(firstFocusableEl, 'blur', () => {
-		if (active && inCurrentTrap()) {
-			shiftTab && lastFocusableEl.focus()
+	/** loop focus if leaving first of last focusable element in wrap */
+	const focusOutListener = listen(wrap, 'focusout', (e: FocusEvent) => {
+		if (inCurrentTrap(wrap)) {
+			const [firstFocusableEl, lastFocusableEl] = getFirstAndLastFocusable()
+			if (e.target == firstFocusableEl && shiftTab) {
+				lastFocusableEl.focus()
+			} else if (e.target == lastFocusableEl && !shiftTab) {
+				firstFocusableEl.focus()
+			}
 		}
 	})
-	/** focus first element if focus leaves last element without shift */
-	const lastElBlurListener = listen(lastFocusableEl, 'blur', () => {
-		if (active && inCurrentTrap()) {
-			!shiftTab && firstFocusableEl.focus()
-		}
-	})
 
-	/** listener for focus event, moves focus to container if away */
+	/** moves focus back to wrap if something outside the wrap is focused */
 	const focusListener = listen(document, 'focusin', (e: FocusEvent) => {
-		if (active && !inCurrentTrap(e.target as HTMLElement)) {
-			let focusEl = shiftTab ? lastFocusableEl : firstFocusableEl
+		if (inCurrentTrap(wrap) && !inCurrentTrap(e.target as HTMLElement)) {
+			const [first, last] = getFirstAndLastFocusable()
+			let focusEl = shiftTab ? last : first
 			focusEl.focus()
 		}
 	})
 
 	return {
-		update(options: TrapOptions) {
-			active = options.active
+		/** Enables / disables trap */
+		update(active: boolean) {
+			if (active) {
+				addToStack()
+			} else {
+				removeFromStack()
+			}
 		},
+		/** Destroys trap and removes event listeners */
 		destroy() {
-			// remove listeners
 			focusListener()
-			firstElBlurListener()
-			lastElBlurListener()
-			// remove from stack & focus previously focused element
-			stack = stack.filter((el) => el !== wrap)
-			lastActiveElement.focus()
+			focusOutListener()
+			removeFromStack()
 		},
 	}
 }
